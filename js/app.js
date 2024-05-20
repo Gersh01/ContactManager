67e4c39e13351bb681c3fb3f74c021cc97e47391
jsalzer312 <salzer.james@yahoo.com>
2024-05-20 19:18:17 -0400

    Updated app.js
    - created a barebones template of all the functions needed to allow functionality on the app

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const urlBase = 'http://oceanic-connections.xyz/LAMPAPI';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin(){
+
+
+}
+
+function saveCookie(){
+
+
+}
+
+function readCookie(){
+
+
+}
+
+function doLogout(){
+
+
+
+}
+
+function doRegister(){
+
+
+
+}
+
+function addContact(){
+
+
+
+}
+
+function deleteContact(){
+
+
+
+}
+
+
+function searchContact(){
+
+
+
+}
+
+function updateContact(){
+
+
+
+}
+
+
+function doPagenation(){
+
+
+    
+}
