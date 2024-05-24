b29e1197b007f6f567c38b17f4f11877efa17605
jsalzer312 <salzer.james@yahoo.com>
2024-05-24 19:21:10 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -351,13 +351,13 @@ function firstPage(){
 				let jsonObject = JSON.parse( xhr.responseText )
 				console.log(jsonObject);
 				console.log(typeof(jsonObject));
-				console.log("Length of JSON object "+jsonObject.length);
+				console.log("Length of JSON object "+jsonObject.contacts.length);
 				//loadContacts(jsonObject);
 				if(jsonObject.length == 0){
 					document.getElementById("contact-result").innerHTML = "No contacts found";
 					return;
 				}
-				for(let i = 0; i<jsonObject.length-1; i++){
+				for(let i = 0; i<jsonObject.contacts.length-1; i++){
 					console.log(jsonObject[i].name);
 					document.getElementById("contact-row-1").getElementById("contact-first-name").innerHTML = jsonObject[i].name;
 				}
