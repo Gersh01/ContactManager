b747d8f63a67fa4e1090cfb2dbc2a610766b2b80
alperenyazmaci <38111663+alperenyazmaci@users.noreply.github.com>
2024-05-23 20:57:45 -0400

    Update ContactPagination.php

--- LAMPAPI/ContactPagination.php
+++ LAMPAPI/ContactPagination.php
@@ -15,7 +15,8 @@
         echo "Something went wrong\n";
         returnWithError($conn->connect_error);
     } else {
-        $stmt = $conn->prepare("SELECT ID, Name, Phone, Email, UserID, Favorited FROM Contacts WHERE ID > ? ORDER BY ID LIMIT 10");
+        // $stmt = $conn->prepare("SELECT ID, Name, Phone, Email, UserID, Favorited FROM Contacts WHERE ID > ? ORDER BY ID LIMIT 10");
+        $stmt = $conn->prepare("SELECT ID, Name, Phone, Email, UserID, Favorited FROM Contacts ORDER BY ID");
                 $stmt->bind_param("i", $cursor);
                 $stmt->execute();
         $result = $stmt->get_result();
