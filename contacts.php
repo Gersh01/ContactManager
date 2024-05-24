b1fc8611b47b11dee85f412271e201a5c6ec8cdf
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-05-24 17:23:07 -0400

    Update contacts.php
    closed a div

--- contacts.php
+++ contacts.php
@@ -17,7 +17,7 @@
     <body>
         <header id="nav-header" class="text-black nav py-5 bg-accent"> 
             <div id="home-button" class="text-black nav-link" type="button">Home</div>
-            <a id="about-us_button" href="javascript:doLogout()"> <div id="about-us-button" class="button" type="button">Logout</a>
+            <a id="about-us_button" href="javascript:doLogout()"> <div id="about-us-button" class="button" type="button">Logout</div></a>
             <div id="about-link" class="text-black nav-link" type="button">About</div>
         </header>
         <!-- Image Background -->
