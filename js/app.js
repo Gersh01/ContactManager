7d35fb7297718bfb990a69923c394dbc11f8d2d6
jsalzer312 <salzer.james@yahoo.com>
2024-05-24 19:04:26 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -369,6 +369,7 @@ function loadContacts(jsonObject){
 		return;
 	}
 	for(let i = 0; i<jsonObject.length-1; i++){
+		console.log(jsonObject[i].firstName);
 		document.getElementById("contact-row-"+ i + 1).getElementById("contact-first-name").innerHTML = jsonObject[i].contactFirst;
 	}
 
