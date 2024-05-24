c8091eba3ca77e36f29f63a373efa612239fe290
jsalzer312 <salzer.james@yahoo.com>
2024-05-24 12:26:47 -0400

    update app.js
    upper fix

--- js/app.js
+++ js/app.js
@@ -236,7 +236,7 @@ function passwordRegexChecker(){
 	let password = document.getElementById("register-password").value;
 
 	let eightPasswordRegex = new RegExp("^(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,}$");
-	let upperPasswordRegex = new RegExp("^(?[A-Z])");
+	let upperPasswordRegex = new RegExp("^(?=*[A-Z])");
 	let specialPasswordRegex = new RegExp("(?=.*[@$!%*?&])");
 	let numberPasswordRegex = new RegExp("(?=.*[0-9])");
 	let wholePasswordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
