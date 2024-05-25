c8f6c6d62494ef7f7041bf2c19218e3d7938ff4c
GoldenLin9 <goldenlin0909@gmail.com>
2024-05-25 16:52:05 -0400

    Search now has pagination

--- LAMPAPI/Search.php
+++ LAMPAPI/Search.php
@@ -1,34 +1,49 @@
 <?php
 
-	// echo "0";
 
-    // ini_set('display_errors', 1);
-    // ini_set('display_startup_errors', 1);
-    // error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-	// echo "1";
 
     $data = json_decode(file_get_contents('php://input'), true);
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331" ,"COP4331");
 
+	$result = null;
     if ($conn->connect_error) {
 
         returnWithError($conn->connect_error);
 
     } else {
 
-        $stmt = null;
+		$stmt = null;
 
-        if ($data["showFavorites"]) {
-            $stmt = $conn->prepare("SELECT * FROM Contacts WHERE (Name like ? OR Phone like ? OR Email like ?) AND UserID = ? AND Favorited = 1 ORDER BY Favorited DESC");
-        } else {
-            $stmt = $conn->prepare("SELECT * FROM Contacts WHERE (Name like ? OR Phone like ? OR Email like ?) AND UserID = ? ORDER BY Favorited DESC");
-        }
+		// load first page for new query
+		if ($data["cursor"]["firstName"] == "") {
+			$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Favorited >= ? AND (Name like ? OR Phone like ? OR Email like ?) AND UserID = ? ORDER BY Favorited DESC, Name ASC, ID ASC LIMIT 10");
+
+			$stmt->bind_param("sssss", $data["showFavorites"], $search, $search, $search, $data["UserID"]);
+			
+		} else {
+			// not loading the first page
+			$name = $data["cursor"]["firstName"] . ' ' . $data["cursor"]["lastName"];
+			$search = "%" . $data["search"] . "%";
+			
+			// going onto next page
+			if ($data["next"]) {
+				$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Favorited >= ? AND (Name like ? OR Phone like ? OR Email like ?) AND ((Favorited < ?) or (Favorited = ? and Name > ?) or (Favorited = ? and Name = ? and ID > ?)) AND UserID = ? ORDER BY Favorited DESC, Name ASC, ID ASC LIMIT 10");
+				
+			} else {
+				// going to previous page
+				$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Favorited >= ? AND (Name like ? OR Phone like ? OR Email like ?) AND ((Favorited > ?) or (Favorited = ? and Name < ?) or (Favorited = ? and Name = ? and ID < ?)) AND UserID = ? ORDER BY Favorited DESC, Name ASC, ID ASC LIMIT 10");
+			}
+
+			
+			$stmt->bind_param("sssssssssss", $data["showFavorites"], $search, $search, $search, $data["cursor"]["favorite"], $data["cursor"]["favorite"], $name, $data["cursor"]["favorite"], $name, $data["cursor"]["ID"], $data["UserID"]);
+		}
 
 
-        $search = "%" . $data["search"] . "%";
-		$stmt->bind_param("ssss", $search, $search, $search, $data["UserID"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
@@ -37,21 +52,20 @@
         $searchResults = "";
 
 		while($row = $result->fetch_assoc()) {
-			if ($results !== 0) {
+			if ($results > 0) {
 				$searchResults .= ",";
-			} else {
-				$results++;
 			}
+			
+			$results++;
 
 			$parts = explode(" ", $row["Name"]);
 
-			$searchResults .= '{"FirstName" : "' . $parts[0] . '", "LastName" : "' . $parts[1] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '", "Favorite" : "' . $row["Favorited"] . '"}';
+			$searchResults .= '{' . '"ID": "' . $row["ID"] .  '", "FirstName" : "' . $parts[0] . '", "LastName" : "' . $parts[1] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '","UserID" : "' . $row["UserID"] . '", "Favorite" : "' . $row["Favorited"] . '"}';
 		}
 
         if ($results == 0) {
-            returnWithError("No Records Found");
+			returnWithInfo("");
         } else {
-			echo $searchResults;
             returnWithInfo($searchResults);
         }
 
@@ -64,15 +78,9 @@
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
 	function returnWithInfo( $searchResults )
 	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		$retValue = '{"contacts":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
