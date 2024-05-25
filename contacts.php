78b6e816155cf7c940cfd055e6af5028c633011b
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-05-25 16:27:22 -0400

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -36,7 +36,7 @@
                         <div id="search-bar-row" class="search-bar row-alt">
                             <input id="search-bar" type="text" placeholder="Search..." required onkeyup = "searchContact()">
                             <a id="refresh-button" href="javascript:firstPage()" class="text-black a-link"><div id="refreshButton" class="text-black refresh-page" type="button">Refresh</div></a>
-                            <a id="add-contact-button" href="javascript:toggleElement()" class="text-black a-link"><div id="addContactButton" class="text-black refresh-page" type="button">Add Contact</div></a>
+                            <a id="add-contact-button" href="javascript:showTable()" class="text-black a-link"><div id="addContactButton" class="text-black refresh-page" type="button">Add Contact</div></a>
                         </div>
                     </div>
 
