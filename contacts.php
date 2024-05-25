8fc17886d7ba2920d80363a7956ce9636b8e8a51
JPEACH34 <146260661+JPEACH34@users.noreply.github.com>
2024-05-24 20:26:38 -0400

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -34,7 +34,7 @@
                     <div class="text-center my-6">
                         <div id="search-bar" class="search-bar row-alt">
                             <input type="text" placeholder="Search...">
-                            <a id="refresh-button" href="javascript:firstPage()" class="text-black a-link"><div id="refreshButton" class="text-black refresh-page" type="button">Refresh</div></a>
+                            
                         </div>
                     </div>
 
@@ -136,6 +136,7 @@
                 <body onload= "firstPage()">
                 <!--TODO: FINISH PAGINATION-->
                 <div id="page-selector" class="row-alt justify-content-end">
+                    <a id="refresh-button" href="javascript:firstPage()" class="text-black a-link"><div id="refreshButton" class="text-black refresh-page" type="button">Refresh</div></a>
                     <div id="backButton" class="move-page" type="button">Previous</div>
                     <div id="forwardButton" class="move-page" type="button">Next</div>
                 </div>
