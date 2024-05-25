8de28edc1fedd0a9ec8ba087e0220d59ec1c44ea
alperenyazmaci <38111663+alperenyazmaci@users.noreply.github.com>
2024-05-25 11:37:47 -0400

    Update Search.php

--- LAMPAPI/Search.php
+++ LAMPAPI/Search.php
@@ -4,15 +4,24 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+	echo "1";
+
     $data = json_decode(file_get_contents('php://input'), true);
+
+	echo "2";
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331" ,"COP4331");
 
+	echo "3";
+
     if ($conn->connect_error) {
 
+		echo "if";
         returnWithError($conn->connect_error);
 
     } else {
+
+		echo "else";
         $stmt = null;
 
         if ($data["showFavorites"]) {
@@ -21,16 +30,22 @@
             $stmt = $conn->prepare("SELECT * FROM Contacts WHERE (Name like ? OR Phone like ? OR Email like ?) AND UserID = ? ORDER BY Favorited DESC");
         }
 
+		echo "4";
+
         $search = "%" . $data["search"] . "%";
 		$stmt->bind_param("ssss", $search, $search, $search, $data["UserID"]);
 		$stmt->execute();
+
+		echo "5";
 		
 		$result = $stmt->get_result();
+		
+		echo "6";
 		
 		$results = 0;
         $searchResults = "";
 
-		echo "1";
+		echo "7";
 
 		while($row = $result->fetch_assoc()) {
 			if ($results !== 0) {
