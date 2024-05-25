309580f21f314fce3a85553d93b73a7653cb0620
jsalzer312 <salzer.james@yahoo.com>
2024-05-24 20:55:28 -0400

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -2,8 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
     <!-- On load event to check for cookies -->
-    <body onload= "readCookie()">
     <body onload= "firstPage()">
+    <body onload= "readCookie()">
+    
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
