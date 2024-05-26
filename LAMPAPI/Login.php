6d24a95a4eb72adfd4c28302a692025efe997401
alperenyazmaci <38111663+alperenyazmaci@users.noreply.github.com>
2024-05-26 13:59:11 -0400

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -2,9 +2,9 @@
 
     echo "123";
 
-    // ini_set('display_errors', 1);
-    // ini_set('display_startup_errors', 1);
-    // error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     $id = 0;
     $firstName = "";
