e768a529cf51791d66c68e23efe73da77df388db
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-05-29 23:19:40 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //const urlBase = 'http://159.203.115.181/LAMPAPI';
-const urlBase = 'http://143.198.9.78/LAMPAPI';
+const urlBase = 'http://oceanic-connections.xyz/LAMPAPI';
 
 const extension = 'php';
 
