f52543d6eabc5c6f46b29c113f8da835750e8153
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-06-13 13:11:30 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -978,33 +978,31 @@ function deleteContact(num){
 			xhr.open("POST", url, true);
 			xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 			console.log(jsonPayload);
-			if(confirm("Are you sure you want to delete this contact?") == true) {
-				try{
-					xhr.onreadystatechange = function() {
-						if(this.readyState == 4 && this.status == 200){
-							let jsonObject = JSON.parse(xhr.responseText);
-							console.log(jsonObject.deleted);
-							console.log(jsonObject.error);
-							if(jsonObject.deleted === "Yes"){
-								document.getElementById("contact-result").innerHTML = "Contact has been deleted";
-								if(document.getElementById("search-bar").value == ""){
-									firstPage(null,null);
-								}
-								else{
-									searchContact(null,null,null,null,null);
-								}
+			try{
+				xhr.onreadystatechange = function() {
+					if(this.readyState == 4 && this.status == 200){
+						let jsonObject = JSON.parse(xhr.responseText);
+						console.log(jsonObject.deleted);
+						console.log(jsonObject.error);
+						if(jsonObject.deleted === "Yes"){
+							document.getElementById("contact-result").innerHTML = "Contact has been deleted";
+							if(document.getElementById("search-bar").value == ""){
+								firstPage(null,null);
 							}
 							else{
-								document.getElementById("contact-result").innerHTML = "Contact was not deleted";
+								searchContact(null,null,null,null,null);
 							}
 						}
-					};
-					xhr.send(jsonPayload);
-				}
-				catch(err){
-					document.getElementById("contact-result").innerHTML = err.message;
-					console.log(err.message);
-				}
+						else{
+							document.getElementById("contact-result").innerHTML = "Contact was not deleted";
+						}
+					}
+				};
+				xhr.send(jsonPayload);
+			}
+			catch(err){
+				document.getElementById("contact-result").innerHTML = err.message;
+				console.log(err.message);
 			}
 		}
 	}
