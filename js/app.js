2b02473d203b67207a915043768df55e2d46cd7c
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-06-13 12:11:11 -0400

    Add confirm delete

--- js/app.js
+++ js/app.js
@@ -954,53 +954,55 @@ function searchContact(first, last, contactId, favorite, pagination){
 }
 
 function deleteContact(num){
-	if(contactInEdit === 0){
-		console.log(num);
-
-		let url = urlBase + "/DeleteContact." + extension;
-
-		//globalJsonObject.splice(num-1,1);
-		console.log(globalJsonObject);
-
-		let deletedContactID = globalJsonObject.contacts[num-1].ID;
-		let convertToString = "" + deletedContactID;
-
-		let tmp = {contactID:deletedContactID};
-		
-		let jsonPayload = JSON.stringify(tmp);
-
-		console.log(convertToString);
-
-		let xhr = new XMLHttpRequest();
-
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-		console.log(jsonPayload);
-		try{
-			xhr.onreadystatechange = function() {
-				if(this.readyState == 4 && this.status == 200){
-					let jsonObject = JSON.parse(xhr.responseText);
-					console.log(jsonObject.deleted);
-					console.log(jsonObject.error);
-					if(jsonObject.deleted === "Yes"){
-						document.getElementById("contact-result").innerHTML = "Contact has been deleted";
-						if(document.getElementById("search-bar").value == ""){
-							firstPage(null,null);
+	if(confirm("Are you sure you want to delete this contact?")) {
+		if(contactInEdit === 0){
+			console.log(num);
+	
+			let url = urlBase + "/DeleteContact." + extension;
+	
+			//globalJsonObject.splice(num-1,1);
+			console.log(globalJsonObject);
+	
+			let deletedContactID = globalJsonObject.contacts[num-1].ID;
+			let convertToString = "" + deletedContactID;
+	
+			let tmp = {contactID:deletedContactID};
+			
+			let jsonPayload = JSON.stringify(tmp);
+	
+			console.log(convertToString);
+	
+			let xhr = new XMLHttpRequest();
+	
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+			console.log(jsonPayload);
+			try{
+				xhr.onreadystatechange = function() {
+					if(this.readyState == 4 && this.status == 200){
+						let jsonObject = JSON.parse(xhr.responseText);
+						console.log(jsonObject.deleted);
+						console.log(jsonObject.error);
+						if(jsonObject.deleted === "Yes"){
+							document.getElementById("contact-result").innerHTML = "Contact has been deleted";
+							if(document.getElementById("search-bar").value == ""){
+								firstPage(null,null);
+							}
+							else{
+								searchContact(null,null,null,null,null);
+							}
 						}
 						else{
-							searchContact(null,null,null,null,null);
+							document.getElementById("contact-result").innerHTML = "Contact was not deleted";
 						}
 					}
-					else{
-						document.getElementById("contact-result").innerHTML = "Contact was not deleted";
-					}
-				}
-			};
-			xhr.send(jsonPayload);
-		}
-		catch(err){
-			document.getElementById("contact-result").innerHTML = err.message;
-			console.log(err.message);
+				};
+				xhr.send(jsonPayload);
+			}
+			catch(err){
+				document.getElementById("contact-result").innerHTML = err.message;
+				console.log(err.message);
+			}
 		}
 	}
 }
